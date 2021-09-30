868f7acf8f808cc9c9f8b341d99fbddb1a4646f9
louaysaidi22 <louaysaidi18@gmail.com>
2021-09-30 11:39:46 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
-int main(void)
+/*
+ * C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ */
+int main(void)	
 {
-	puts("''Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
