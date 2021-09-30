19c65fe87ee417aa369586dc7230a1fe961858ad
louaysaidi22 <louaysaidi18@gmail.com>
2021-09-30 15:49:26 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of a char: %lu bytes(s)\n", sizeof(c));
 printf("Size of an int: %lu bytes(s)\n", sizeof(a));
 printf("Size of a long int: %lu bytes(s)\n", sizeof(la));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(lla));
-printf("Size of a float: %lu byte(s)\n", sizeof(lla));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
 return (0);
 }
