dd7cbc0c4f3137fb8f18edc27e9310c310162e15
louaysaidi22 <louaysaidi18@gmail.com>
2021-09-30 12:03:51 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
 #include<stdio.h>
-int main(void)
+int main(void) /*C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.*/ 
 {puts("\"Programming is like building a multilingual puzzle");
 return (0); }
