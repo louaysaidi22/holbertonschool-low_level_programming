a815003fda997c24cafc199566277cf84a687ffd
louaysaidi22 <louaysaidi18@gmail.com>
2021-09-30 15:41:19 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,13 @@
  */
 int main(void)
 {
-printf("%lu\n", sizeof();
+char c;
+int a;
+long int la;
+long long int lla; 
+printf("Size of char: %lu bytes(s)\n",sizeof(c));
+printf("Size of int: %lu bytes(s)\n",sizeof(a));
+printf("Size of long int: %lu bytes(s)\n",sizeof(la));
+printf("Size of long long int: %lu byte(s)\n",sizeof(lla));
 return (0);
 }
