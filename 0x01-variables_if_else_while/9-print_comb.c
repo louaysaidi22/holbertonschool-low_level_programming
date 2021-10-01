0ea53f9120612ef0ee720d228c6adc7dc3f377e8
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-01 12:17:51 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,16 @@
 int main(void)
 {
 int n = 0;
-while (n < 9)
+while (n < 10)
 {
 putchar(n + '0');
+if (n != 9)
+{
 putchar(',');
 putchar(' ');
+}
 n++;
 }
-n = 9;
-putchar(n + '0');
+putchar('\n');
 return (0);
 }
