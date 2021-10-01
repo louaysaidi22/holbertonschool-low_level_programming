f67768332f2286580e95fb0309fe1aa3eb2934fe
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-01 11:30:07 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *  prints all possible combinations of single-digit numbers..
+ * Return: 0
+ */
+int main(void)
+{
+int n = 0;
+while (n < 9)
+{
+printf("%d", n);
+printf(", ");
+n++;
+}
+n = 9;
+printf("%d", n);
+return (0);
+}
