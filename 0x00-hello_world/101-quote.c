9f08ef2f01ad873b67f46289abf5af426e058b0d
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-04 02:41:31 -0700

    task9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 /**
@@ -8,6 +9,6 @@
 int main(void)
 {
 char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-syscall(SYS_write, 1, msg, sizeof(msg));
+syscall(SYS_write, 0, msg, sizeof(msg));
 return (1);
 }
