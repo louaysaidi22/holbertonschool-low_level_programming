3ea15538fbade4ea93af4b3fc1af7ee96e214a9e
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-04 05:48:59 -0700

    task12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *  prints all possible combinations of two two-digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+int n = '0';
+int d = '0';
+int m = '0';
+int p = '0';
+while (n <= '9')
+{
+while (d <= '9')
+{
+while (m <= '9')
+{
+while (p <= '9')
+{
+if (((n != m) || (d != p)) && ((m == n) && (d < p)))
+{
+putchar(n);
+putchar(d);
+putchar(' ');
+putchar(m);
+putchar(p);
+if ()
+{
+putchar(',');
+putchar(' ');
+}
+}
+p++;
+}
+p = '0';
+m++;
+}
+m = '0';
+d++;
+}
+d = '0';
+n++;
+}
+putchar('\n');
+return (0);
+}
