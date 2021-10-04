9f1f797e7331c6422df122071be55d159e3c3d83
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-04 07:18:16 -0700

    task9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
  */
 int main(void)
 {
-char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 syscall(SYS_write, 1, msg, sizeof(msg));
+perror("\n");
 return (1);
 }
