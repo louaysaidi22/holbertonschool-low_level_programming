eb9597d1fdd0290ebbaa09301a9b760a31f25921
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-05 12:24:32 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,9 +11,10 @@ int i = 0;
 int j = 0;
 int k = 0;
 int l = 0;
+int c = 10;
 while (i < 2)
 {
-while (j < 10)
+while (j < c)
 {
 while (k < 6)
 {
@@ -35,28 +36,9 @@ k = 0;
 }
 i++;
 j = 0;
-}
-while (j < 4)
+if (i == 2)
 {
-while (k < 6)
-{
-while (l < 10)
-{
-_putchar(i + '0');
-_putchar(j + '0');
-_putchar(':');
-_putchar(k + '0');
-_putchar(l + '0');
-_putchar('\n');
-l++;
+c = 4;
 }
-k++;
-l = 0;
-}
-j++;
-k = 0;
-}
-i++;
-j = 0;
 }
 }
