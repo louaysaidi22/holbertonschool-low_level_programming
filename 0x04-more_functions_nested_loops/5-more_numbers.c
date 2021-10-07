ff1dae6864a246e3ebfa46dd5caf38b900a383a7
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-07 11:05:24 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,18 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, l, k;
 
-	for (j = 0; i <= 10; j++)
+	for (j = 0; j <= 10; j++)
 	{
 		for (i = 0; i < 15; i++)
-		{
-			_putchar(i + 48);
+		{	l = i / 10;
+			k = i % 10;
+			if (i > 9)
+			{
+				_putchar(l + 48);
+			}
+			_putchar(k + 48);
 		}
 		_putchar('\n');
 
