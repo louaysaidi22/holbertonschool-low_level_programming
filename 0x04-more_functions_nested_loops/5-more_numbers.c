44ea1a3b90e6aaf1104ceb9cfe752077ac5cb563
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-07 10:55:07 -0700

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers -  function that prints 10 times
+ * the numbers, from 0 to 14, followed by a new line.
+ */
+
+void more_numbers(void)
+{
+	int i,j;
+	for (j = 0; i <= 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			_putchar(i + 48);
+		}
+		_putchar('\n');
+
+	}
+}
+
