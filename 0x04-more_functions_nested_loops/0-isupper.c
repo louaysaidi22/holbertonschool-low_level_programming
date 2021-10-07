c91599dff9514e710f8e380c2d336459f8365aeb
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-07 05:29:29 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,13 @@
 /**
  * _isupper -  function that checks
  * if the character c is upper case or not
- * @c: using l to return 1 or 0
+ * @c: uppercse checker
  * Return: 1 if c is uppercase otherwise 0
  */
 int _isupper(int c)
 {
-	int l = 0;
-
-	if ((c >= 'A') && (c <= 'Z'))
-	{
-		l = 1;
-	}
-	return (l);
+	if (c > 64 && c < 91)
+		return(1);
+	else
+		return (0);
 }
