c337771b297b9fe8e5ea26269a212e0e2dc0b5b5
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-07 12:42:08 -0700

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 	{
 	int n;
 
-	for (n = 1; n <= 100; n++)
+	for (n = 1; n < 100; n++)
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf("FizzBuzz ");
@@ -28,5 +28,6 @@ int main(void)
 		}
 		else
 			printf("%i ", n);
+	printf("Buzz\n");
 	return (0);
 	}
