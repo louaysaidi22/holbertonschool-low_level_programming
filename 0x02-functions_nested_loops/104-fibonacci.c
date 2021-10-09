98e3722e9043e71ef8a897b14f62243312575c5c
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-09 09:03:02 -0700

    task16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints the first 98 Fibonacci numbers
+ * starting with 1 and 2, followed by a new line.
+ * Return: 0
+ **/
+int main(void)
+{
+int i;
+long int p = 1;
+long int n = 2;
+long int f = 0;
+printf("%lu, ", p);
+printf("%lu, ", n);
+for (i = 1; i < 96; i++)
+{
+f = n + p;
+printf("%lu, ", f);
+p = n;
+n = f;
+}
+f = n + p;
+printf("%lu\n", f);
+return (0);
+}
