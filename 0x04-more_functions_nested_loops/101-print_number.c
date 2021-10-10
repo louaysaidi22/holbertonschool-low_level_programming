c68e0ad0a819f631b4ff96518f59946e61015118
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-10 12:26:09 -0700

    task12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,21 +7,13 @@
 
 void print_number(int n)
 {
-	int i, j;
-
 	if (n < 0)
 	{
-		n = n * -1;
-		putchar('-');
+		n = -n;
+		_putchar('-');
 	}
-	i = 1;
-	while (n % 10 != 0)
-	{
-		i++;
-	}
-	for (j = 0; j < i; j++)
-	{
-		_putchar(n % (10 (**) (i - j)) + 48);
-	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + 48);
 }
 
