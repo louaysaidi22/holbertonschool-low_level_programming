0af9098ffa78b22bac3e20bcbe784bab8a269df9
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-11 05:45:31 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,17 +6,17 @@
  */
 void print_rev(char *s)
 {
-	int i, j, l;
+	int i, j, k, l;
+	char rev[] = "";
 
 	l = 0;
 	while (*(s + l) != '\0')
 	{
 	l++;
 	}
-	char rev[l];
 	for (i = l - 1; i >= 0; i--)
 	{
-		rev + j = *(s + i);
+		(rev + j) = *(s + i);
 		j++;
 	}
 	for (k = 0; k < l; k++)
