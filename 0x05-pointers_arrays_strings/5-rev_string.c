4c5254e6a9ad96d38f0cf944ef655fb4c7e91399
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-11 07:16:29 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,12 +7,13 @@ void rev_string(char *s)
 {
 	int l, i;
 	char temp;
+
 	l = 0;
 	while (*(s + l) != '\0')
 	{
 	l++;
 	}
-	for (i = l - 1; i > l / 2; i--)
+	for (i = l - 1; i >= l / 2; i--)
 	{
 		temp = s[i];
 		s[i] = s[l - i - 1];
