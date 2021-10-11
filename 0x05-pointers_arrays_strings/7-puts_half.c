6fb2774bef1b8a7c492c86eeccd3566c7aaa9c32
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-11 08:42:55 -0700

    task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,10 +15,15 @@ void puts_half(char *str)
 	if (l % 2 != 0)
 	{
 		l--;
+		for (i = l / 2; i < l; i++)
+		{
+			_putchar(*(str + i));
+		}
 	}
-	for (i = l / 2 + l % 2; i < l; i++)
-	{
-		_putchar(*(str + i));
-	}
+	else
+		for (i = l / 2 + 1; i < l; i++)
+		{
+			_putchar(*(str + i));
+		}
 	_putchar('\n');
 }
