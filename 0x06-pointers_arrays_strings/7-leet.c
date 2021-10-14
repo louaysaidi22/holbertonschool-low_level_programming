d39c28ba2f1e581f12bf82ac1f55020074e6c5a5
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-14 15:16:23 -0700

    task7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * leet -  function that encodes a string into 1337.
+ * @s: string
+ * Return: s
+ */
+char *leet(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char a[5] = {'A', 'E', 'O', 'T', 'L'};
+	char b[5] = {'4', '3', '0', '7', '1'};
+
+	while (s[i])
+	{
+		j = 0;
+		while (b < 5)
+		{
+			if (s[i] == a[j] || s[i] - 32 == a[j])
+			{
+				s[i] = b[i];
+			}
+			j++;
+		}
+		i++;
+	}
+	return(s);
+}
