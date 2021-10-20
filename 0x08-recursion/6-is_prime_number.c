2d9a03c66e467aa22a741ad4d883ea1e1cba9ccf
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-20 14:38:58 -0700

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -22,11 +22,11 @@ int second(int i, int p)
  */
 int is_prime_number(int n)
 {
-	if (n == 1 || n == -1)
+	if (n <= 1)
 	{
 		return (0);
 	}
-	else if (n == 2 || n == -2)
+	else if (n == 2)
 	{
 		return (1);
 	}
