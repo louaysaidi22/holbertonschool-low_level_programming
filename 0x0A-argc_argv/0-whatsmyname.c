aee12cd81e0c7b1a15c7a25433d2feff5ebcd6d2
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-22 10:35:58 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <main.h>
 /**
  * main - Entry point
  * a program that prints its name, followed by a new line.
@@ -7,7 +6,8 @@
  * @argv: argument vector
  * Return: 0
  */
-int main(__attribute__((unused)) int argc,char *argv[])
+int main(int __attribute__((unused)) argc,char *argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
