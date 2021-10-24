d478f59c77d74dd9e75a319c72c5a39e31740f65
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-24 08:09:13 -0700

    task5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * program that prints the minimum number
+ * of coins to make change for an amount of money.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 or 0
+ */
+int main(int argc, char *argv[])
+{
+	int a = atoi(argv[1]);
+	int change = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (a <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (a / 25 != 0)
+	{
+		change++;
+		a -= 25;
+	}
+	while (a / 10 != 0)
+	{
+		change++;
+		a -= 10;
+	}
+	while (a / 5 != 0)
+	{
+		change++;
+		a -= 5;
+	}
+	while (a / 2 != 0)
+	{
+		change++;
+		a -= 2;
+	}
+	if (a == 1)
+	{
+		change++;
+	}
+	printf("%d\n", change);
+	return (0);
+}
