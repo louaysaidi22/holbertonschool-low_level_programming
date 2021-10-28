731f7b722e021ed431a1bb1138a0ff609a6330c3
louaysaidi22 <louaysaidi18@gmail.com>
2021-10-28 22:40:43 +0100

    task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,11 +10,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *n;
+	char *s;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	n = malloc(nmemb * size);
 	if (n == NULL)
 		return (NULL);
+	s = n;
+	for (i = 0; i < nmemb; i++)
+	{
+		s[i] = '\0';
+	}
 	return (n);
 }
