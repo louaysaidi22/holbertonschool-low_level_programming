fdf36da7fe26f2842f48d00668ad0816112f9a14
louaysaidi22 <louaysaidi18@gmail.com>
2021-11-28 19:27:48 +0100

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fo = open(filename, O_CREAT | O_RDWR, 0600);
+	fo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fo == -1)
 		return (-1);
 	if (text_content != NULL)
