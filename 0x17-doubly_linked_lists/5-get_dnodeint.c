535f9af887bbdc5ab14a14838bf9c54a36df77f1
louaysaidi22 <louaysaidi18@gmail.com>
2022-01-08 10:55:03 +0100

    task5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	node = head;
-	while (node->next)
+	while (node)
 	{
 	if (i == index)
 		return (node);
